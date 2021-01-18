aa5f3000af6ad72e14aaa3aea9b95f494c6b1c5d
naveenautomationlabs <naveenautomationlabs@naveenautomationlabss-MacBook-Pro.local>
2021-01-18 17:27:27 +0530

    created profile page java class

---
+++ src/main/java/com/qa/democart/pages/ProfilePage.java
@@ -0,0 +1,16 @@
+package com.qa.democart.pages;
+
+public class ProfilePage {
+
+	public ProfilePage() {
+		System.out.println("PP -- default const...");
+	}
+	
+	public void getTitle() {
+		System.out.println("PP - title");
+	}
+	
+	
+	
+	
+}
