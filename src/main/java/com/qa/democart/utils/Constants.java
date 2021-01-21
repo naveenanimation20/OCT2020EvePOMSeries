22535f1e54d0f6132ac8b4024d5d19dd9d5d7a66
naveenautomationlabs <naveenautomationlabs@naveenautomationlabss-MacBook-Pro.local>
2021-01-21 17:23:15 +0530

    updated account sections count test

--- src/main/java/com/qa/democart/utils/Constants.java
+++ src/main/java/com/qa/democart/utils/Constants.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Constants {
 
 	public static final int FORGOTTEN_PWD_LINK_COUNT = 2;
-	public static final int ACCOUNT_SECTIONS_COUNT = 5;
+	public static final int ACCOUNT_SECTIONS_COUNT = 4;
 
 	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String ACCOUNTS_PAGE_TITLE = "My Account";
